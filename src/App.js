43e014e5691b1676100e86a3b0f841462e8a897b
iplukens <iplukens@gmail.com>
2020-09-01 20:07:28 -0400

    remove everything

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import FULL_DECK, {shuffle} from './full_deck'
 
 function CardPile(props) {
   let {cards} = props
-  if (cards.length > 4) {
-    cards = cards.slice(cards.length - 4, cards.length)
-  }
-  const cardPile = cards.map(card => {
-    const cardClass = ['♦️', '♥️'].includes(card.suit) ? 'card red' : 'card'
-    return (
-      <div className={cardClass} key={card.suit + card.value}>
-        <p className="card-value">{card.value}</p>
-        <p className="card-suit">{card.suit}</p>
-      </div>
-    )
-  })
 
-  return <div id="pile" className="cards">{cardPile}</div>
+  return <div id="pile" className="cards">{cards}</div>
 }
 
 function Game() {
@@ -27,41 +15,18 @@ function Game() {
   const [score, setScore] = useState(0)
 
   function draw() {
-    const newCards = [...cards]
-    const newPile = [...pile]  
-    do {    
-      const drawnCard = newCards.shift()    
-      newPile.push(drawnCard)     
-    }
-    while (newPile.length < 4 && newCards.length > 0)
-
-    setCards(newCards)
-    setPile(newPile)
+    
   }
 
   function eliminate() {
-    const newPile = [...pile]
-
-    newPile.splice(newPile.length - 4, 1)
-    newPile.splice(newPile.length -1, 1)
-    setScore(score + 2)
-
-    setPile(newPile)
+    
   }
 
   let canEliminate = false
-  if (pile.length >= 4) {
-    const fourthDown = pile[pile.length - 4],
-          lastCard = pile[pile.length - 1]
-    
-    canEliminate = lastCard.value === fourthDown.value || lastCard.suit === fourthDown.suit
-  }
   const canDraw = cards.length > 0
 
   function restart() {
-    setPile([])
-    setCards(shuffle(FULL_DECK))
-    setScore(0)
+
   }
 
   const isGameOver = !(canDraw || canEliminate)
